eda7a7d059f15ec66b0117c45b6c7bdaf522ff81
yisar <1533540012@qq.com>
2024-04-02 11:20:13 +0800

    emmm

--- App.jsx
+++ App.jsx
@@ -9,17 +9,20 @@ import { Route, Routes, useNavigate } from 'react-router-dom';
 
 const pages = [
   {
+    name:'Uno',
     path: '/',
     comp: lazy(() => import("./pages/uno.jsx")),
     loader: UnoR
   },
   {
+    name:'Doc',
     path: '/doc',
     comp: lazy(() => import("./pages/doc.jsx")),
     loader: DocR
   },
   {
-    path: 'ties',
+    name:'Ties',
+    path: '/ties',
     comp: lazy(() => import("./pages/ties.jsx")),
     loader: TiesR
   }
@@ -29,7 +32,6 @@ import { fetchData } from "./fetchData.js";
 const initialResource = fetchData(UnoR);
 
 export default function App() {
-  const [tab, setTab] = useState(0);
   const [resource, setResource] = useState(initialResource);
   const [isPending, startTransition] = useTransition();
   const navigate = useNavigate();
@@ -37,7 +39,6 @@ export default function App() {
   function handleClick(index) {
     const newRes = fetchData(pages[index].loader)
     startTransition(() => {
-      setTab(index);
       setResource(newRes);
       navigate(pages[index].path)
     });
@@ -47,24 +48,12 @@ export default function App() {
     <>
       <GlobalLoader isLoading={isPending} />
       <ul className="inline">
-        <li
-          className={tab === 0 ? "selected" : null}
-          onClick={() => handleClick(0)}
+        {pages.map((item, index) => <li
+          className={location.pathname === item.path ? "selected" : null}
+          onClick={() => handleClick(index)}
         >
-          Uno
-        </li>
-        <li
-          className={tab === 1 ? "selected" : null}
-          onClick={() => handleClick(1)}
-        >
-          Dos
-        </li>
-        <li
-          className={tab === 2 ? "selected" : null}
-          onClick={() => handleClick(2)}
-        >
-          Tres
-        </li>
+          {item.name}
+        </li>)}
       </ul>
       <div className={`tab ${isPending ? "pending" : null}`}>
         <Suspense fallback={<div>loading...</div>}>
